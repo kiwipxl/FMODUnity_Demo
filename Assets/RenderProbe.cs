e5a8ad66bdb90fa989040ba24368e302168599e9
kiwipxl <kiwipxl@gmail.com>
2015-10-10 13:12:10 +1100

    Initial tank battle project template (with few changges)

---
+++ Assets/RenderProbe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderProbe : MonoBehaviour {
+
+    ReflectionProbe probe;
+
+	void Start () {
+        probe = GetComponent<ReflectionProbe>();
+	}
+
+    void Update () {
+        probe.transform.position = new Vector3(
+            Camera.main.transform.position.x,
+            -Camera.main.transform.position.y,
+            Camera.main.transform.position.z
+        );
+
+        probe.RenderProbe();
+	}
+}
