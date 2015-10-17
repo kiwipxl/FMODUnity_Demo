06a12a09240dfceffb7d61128125c4345badb7fc
FlynnsPixel <flynnspixel@gmail.com>
2015-10-17 13:50:36 +1100

    Moved voer water standard assets

---
+++ Assets/Examples/FMODUnityDemo/Scripts/Water/GerstnerDisplace.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+namespace FMODUnityDemo.Water
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(WaterBase))]
+    public class GerstnerDisplace : Displace { }
+}
