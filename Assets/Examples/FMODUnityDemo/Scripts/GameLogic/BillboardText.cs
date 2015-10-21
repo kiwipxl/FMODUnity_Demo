fdf72dbf6a172effa61e6283b17e34a4817e1ba1
kiwipxl <kiwipxl@gmail.com>
2015-10-21 17:10:37 +1100

    Added billboard text

---
+++ Assets/Examples/FMODUnityDemo/Scripts/GameLogic/BillboardText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardText : MonoBehaviour {
+
+	void Start() {
+	
+	}
+
+    void Update() {
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(0, 180, 0);
+    }
+}
